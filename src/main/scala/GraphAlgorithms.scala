cffb63596e0a5b0b0d76b70ffad73b145e2ebe97
aletcompany <aletcompany@yandex.ru>
2026-06-08 17:51:13 +0300

    simple graph algorithms

---
+++ src/main/scala/GraphAlgorithms.scala
@@ -0,0 +1,45 @@
+type Graph = Map[Int, List[Int]]
+
+object GraphAlgorithms:
+
+  // dfs
+  def dfs(graph: Graph, start: Int, visited: Set[Int] = Set.empty): Unit =
+    if !visited.contains(start) then
+      println(s"DFS visit node: $start")
+      val newVisited = visited + start
+      
+      val neighbors = graph.getOrElse(start, Nil)
+      neighbors.foreach(neighbor => dfs(graph, neighbor, newVisited))
+
+  // bfs
+  @annotation.tailrec
+  def bfs(
+      graph: Graph, 
+      queue: List[Int], 
+      visited: Set[Int] 
+  ): Unit = queue match
+    case Nil => 
+      () 
+      
+    case currentNode :: restOfQueue =>
+      if visited.contains(currentNode) then
+        bfs(graph, restOfQueue, visited)
+      else
+        println(s"BFS visit node: $currentNode")
+        val newVisited = visited + currentNode
+        
+        // Берем соседей, которых еще не посещали, и добавляем в конец очереди
+        val neighbors = graph.getOrElse(currentNode, Nil)
+        val newNeighbors = neighbors.filterNot(newVisited.contains)
+        
+        bfs(graph, restOfQueue ++ newNeighbors, newVisited)
+    
+
+  def printGraph(g: Graph): Unit =
+        println("Graph structure (node -> neighbors):")
+        g.keys.toList.sorted.foreach { node =>
+        val neighbors = g.getOrElse(node, List.empty)
+        println(s"  $node -> [${neighbors.mkString(", ")}]")
+        }
+
+  
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,3 +12,18 @@
   "Uraboras".print
 
   Cat("Garfield", 5, "Orange").print
+
+  // Graphs
+  val graph: Graph = Map(
+    1 -> List(2, 3),
+    2 -> List(1, 4),
+    3 -> List(1, 5),
+    4 -> List(2),
+    5 -> List(3)
+  )
+
+  GraphAlgorithms.printGraph(graph)
+
+  GraphAlgorithms.dfs(graph, 1)
+  
+  GraphAlgorithms.bfs(graph, List(1), Set.empty)
