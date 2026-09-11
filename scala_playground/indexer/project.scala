b4460ba7c2e14b54ed04e96f617044a06e2fcf05
Artemiy <artem.pospelov2018@yandex.ru>
2026-09-11 10:55:02 +0300

    get eth last block hash

--- scala_playground/indexer/project.scala
+++ scala_playground/indexer/project.scala
@@ -1,2 +1,3 @@
 //> using scala "2.13.18"
 //> using dep "io.getquill::quill-sql::4.8.5"
+//> using dep "com.lihaoyi::ujson::4.4.3"
--- scala_playground/indexer/src/src/main/scala/Indexer.scala
+++ scala_playground/indexer/src/src/main/scala/Indexer.scala
@@ -1,4 +1,6 @@
 import io.getquill._
+import java.net.{URI, HttpURLConnection}
+import scala.io.Source
 
 case class Transaction(height: Long, hash: String)
 
@@ -19,5 +21,31 @@ object Indexer extends App {
     println(mirror.string)
 
     println(mirror.prepareRow.data)
+  }
+
+  {
+    val rpcUrl = "https://ethereum-rpc.publicnode.com"
+    // val rpcUrl = "https://rpc.ankr.com/eth"
+    // val rpcUrl = "https://cloudflare-eth.com"
+
+    val requestBody =
+      """{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}"""
+
+    val url = URI.create(rpcUrl).toURL
+    val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+    conn.setRequestMethod("POST")
+    conn.setRequestProperty("Content-Type", "application/json")
+    conn.setRequestProperty("User-Agent", "Mozilla/5.0")
+    conn.setDoOutput(true)
+    conn.getOutputStream.write(requestBody.getBytes("UTF-8"))
+
+    val response = Source.fromInputStream(conn.getInputStream).mkString
+    println(s"Raw response: $response")
+
+    val json = ujson.read(response)
+    val blockNumberHex = json("result").str
+    val blockNumber = java.lang.Long.parseLong(blockNumberHex.drop(2), 16)
+
+    println(s"Current Ethereum block: $blockNumber")
   }
 }
